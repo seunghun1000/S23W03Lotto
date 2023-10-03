32ddf93e3b603e7557b96be88707dc02fc12e316
이승훈 <hun2548@naver.com>
2023-10-03 18:40:09 +0900

    s23w0301Lotto view모델, 라이브데이터, 옵저버

--- app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w03lotto/LottoViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w03lotto/LottoViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 class LottoViewModel : ViewModel( ) {
+    //private var _numbers = intArrayOf(0, 0, 0, 0, 0, 0)
     private var _numbers = MutableLiveData<IntArray>(IntArray(6) { 0 })
     val numbers: LiveData<IntArray>
         get() = _numbers
@@ -14,18 +15,18 @@ class LottoViewModel : ViewModel( ) {
         var num = 0
         val newNumbers = IntArray(6) {0}
         for (i in newNumbers.indices) {
+//            _numbers[i] = Random.nextInt(1, 46)
             // 중복 검사
             do {
                 num = Random.nextInt(1, 46)
             } while (newNumbers.contains(num))
             newNumbers[i] = num
 
-          //  Log.i("Number!!!", _numbers[i].toString())
+        //   Log.i("Number!!!", _numbers[i].toString())
         }
 
         // 정렬
         newNumbers.sort()
-
         _numbers.value = newNumbers
     }
 }
--- app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w03lotto/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w03lotto/MainActivity.kt
@@ -2,25 +2,72 @@ package kr.ac.kumoh.ce.s20100976.s23w03lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20100976.s23w03lotto.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
+    private lateinit var model: LottoViewModel
+    private lateinit var txtNum: Array<TextView?>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Lifecycle!!!", "onCreate")
         //setContentView(R.layout.activity_main)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
+        txtNum = arrayOf(main.num1, main.num2, main.num3, main.num4, main.num5, main.num6)
+
+        model = ViewModelProvider(this)[LottoViewModel::class.java]
+        setNumbersText()
+
+        model.numbers.observe(this, Observer {
+            Log.i("Observer!!!", "numbers changed")
+            setNumbersText()
+        })
+
         main.btnGenerate.setOnClickListener {
-            main.num1.text = Random.nextInt(1, 46).toString()
-            main.num2.text = Random.nextInt(1, 46).toString()
-            main.num3.text = Random.nextInt(1, 46).toString()
-            main.num4.text = Random.nextInt(1, 46).toString()
-            main.num5.text = Random.nextInt(1, 46).toString()
-            main.num6.text = Random.nextInt(1, 46).toString()
+            model.generate()
+        //    setNumbersText()
         }
+    }
+    private fun setNumbersText(){
+        txtNum.forEachIndexed { index, textView ->
+            textView?.text = model.numbers.value!![index].toString()
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle!!!", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle!!!", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle!!!", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle!!!", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle!!!", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle!!!", "onDestroy")
     }
 }
