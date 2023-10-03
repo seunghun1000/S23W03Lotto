00a299946d66004ff2adc4ea0d6f8326740d4e48
이승훈 <hun2548@naver.com>
2023-10-03 18:16:27 +0900

    s23w0301Lotto 세로방향

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w03lotto/LottoViewModel.kt
@@ -0,0 +1,31 @@
+package kr.ac.kumoh.ce.s20100976.s23w03lotto
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class LottoViewModel : ViewModel( ) {
+    private var _numbers = MutableLiveData<IntArray>(IntArray(6) { 0 })
+    val numbers: LiveData<IntArray>
+        get() = _numbers
+    fun generate() {
+        var num = 0
+        val newNumbers = IntArray(6) {0}
+        for (i in newNumbers.indices) {
+            // 중복 검사
+            do {
+                num = Random.nextInt(1, 46)
+            } while (newNumbers.contains(num))
+            newNumbers[i] = num
+
+          //  Log.i("Number!!!", _numbers[i].toString())
+        }
+
+        // 정렬
+        newNumbers.sort()
+
+        _numbers.value = newNumbers
+    }
+}
--- app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w03lotto/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w03lotto/MainActivity.kt
@@ -5,13 +5,15 @@ import android.os.Bundle
 import kr.ac.kumoh.ce.s20100976.s23w03lotto.databinding.ActivityMainBinding
 import kotlin.random.Random
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
+
         main.btnGenerate.setOnClickListener {
             main.num1.text = Random.nextInt(1, 46).toString()
             main.num2.text = Random.nextInt(1, 46).toString()
